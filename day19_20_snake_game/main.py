06f9211413e7982a1b99afa63ce35ffdcc796728
elaverdiere1 <66510553+elaverdiere1@users.noreply.github.com>
2021-02-28 20:21:05 -0700

    Create main.py

---
+++ day19_20_snake_game/main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+import time
+from food import Food
+from score import Score
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move_snake()
+
+    if snake.snake_list[0].distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    if snake.snake_list[0].xcor() > 280 or snake.snake_list[0].xcor() < -280 or snake.snake_list[0].ycor() > 280 or snake.snake_list[0].ycor() < -280:
+        score.game_over()
+        game_is_on = False
+
+    for snakes in snake.snake_list[1:]:
+        if snake.snake_list[0].distance(snakes) < 10:
+            score.game_over()
+            game_is_on = False
+
+
+screen.exitonclick()
