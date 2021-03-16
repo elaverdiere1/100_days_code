709b9ea78597bc1e706980f6353a350d7942d165
elaverdiere1 <66510553+elaverdiere1@users.noreply.github.com>
2021-03-16 15:15:35 -0600

    Update main.py

--- day24_NATO_alphabet_converter/main.py
+++ day24_NATO_alphabet_converter/main.py
@@ -4,8 +4,17 @@ data = pd.read_csv('nato_phonetic_alphabet.csv')
 data_dict = {row.letter: row.code for (index,row) in data.iterrows()}
 print(data_dict)
 
-user_word = input('Enter a word: ')
-answer = [data_dict[letter] for letter in user_word.upper()]
-print(answer)
+def generate_phonetic():
+    user_word = input('Enter a word: ')
+
+    try:
+        answer = [data_dict[letter] for letter in user_word.upper()]
+    except KeyError:
+        print('Only use letters in the alphabet.')
+        generate_phonetic()
+    else:
+        print(answer)
+
+generate_phonetic()
 
 
