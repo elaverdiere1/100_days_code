9455ce933712f0fcc668e6320a1c7e1c1c39812c
elaverdiere1 <66510553+elaverdiere1@users.noreply.github.com>
2021-03-25 17:47:26 -0600

    Add files via upload

---
+++ Day33_quizzler_gui/data.py
@@ -0,0 +1,11 @@
+import requests
+
+parameters = {
+    'amount': 10,
+    'type': 'boolean',
+}
+
+questions = requests.get("https://opentdb.com/api.php", params=parameters)
+questions.raise_for_status()
+data = questions.json()
+question_data = data['results']
